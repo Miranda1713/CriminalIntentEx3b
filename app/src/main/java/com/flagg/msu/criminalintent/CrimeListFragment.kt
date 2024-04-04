ae67a0a277c3aaedc6a6085c60a9eb509dddf41c
mirandaflagg <mjf846s@missouristate.edu>
2024-04-04 15:28:57 -0500

    CrimeListFragment now working commit

--- app/src/main/java/com/flagg/msu/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/flagg/msu/criminalintent/CrimeListFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.ViewModels
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.flagg.msu.criminalintent.databinding.FragmentCrimeListBinding
 
