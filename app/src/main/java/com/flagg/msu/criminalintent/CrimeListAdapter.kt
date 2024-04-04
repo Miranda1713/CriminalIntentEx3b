624d3020596a88565d69ecdd6444cdbb3f50ffe5
mirandaflagg <mjf846s@missouristate.edu>
2024-04-04 09:40:59 -0500

    RecyclerView commit

---
+++ app/src/main/java/com/flagg/msu/criminalintent/CrimeListAdapter.kt
@@ -0,0 +1,10 @@
+package com.flagg.msu.criminalintent
+
+import androidx.recyclerview.widget.RecyclerView
+import com.flagg.msu.criminalintent.databinding.ListItemCrimeBinding
+
+class CrimeHolder(
+    val binding: ListItemCrimeBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+}
