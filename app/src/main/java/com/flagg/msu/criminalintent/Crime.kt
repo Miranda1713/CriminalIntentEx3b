1be15e1d5b70c8ee4a09c0101bf0b10b0591dad9
mirandaflagg <mjf846s@missouristate.edu>
2024-04-04 00:39:20 -0500

    initial commit

---
+++ app/src/main/java/com/flagg/msu/criminalintent/Crime.kt
@@ -0,0 +1,11 @@
+package com.flagg.msu.criminalintent
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
